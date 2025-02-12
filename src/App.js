f4b7ba3d7d3472c184062e1256711541d61221bb
Mariana-S-Sanches <marianasecchisanches@hotmail.com>
2025-02-12 16:03:12 -0300

    o inicio

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React ainda é pior que flutter :D
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
