e578df44f16bdf5378edbaa57b9bef329ad59b94
Mariana-S-Sanches <marianasecchisanches@hotmail.com>
2025-03-03 15:28:58 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { HoverButton, HeaderWhite } from './components/component';
+import { greenText, whiteText } from './styles/styles';
+import { OutlinedGreenButton, FilledGreenButton } from './styles/button_styles';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React ainda é pior que flutter :D
+        <HeaderWhite title="Plantando tecnologia, colhendo resultados" />
+        <div style={{ display: 'flex', gap: '16px' }}>
+          <button style={OutlinedGreenButton}
+            onClick={() => console.log("Entro em contato")}>Entrar em contato</button>
+          <button style={FilledGreenButton}
+            onClick={() => console.log("Botao doiss")}>Criar conta</button>
+        </div>
+        <p style={greenText}>
+          nav bar
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
+        <p style={whiteText}>
+          nav bar
+        </p>
+        <HoverButton title="Início" href="#start"/>
       </header>
     </div>
   );
 }
 
 export default App;
+
